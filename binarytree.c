b713c3451958e3b3fc6432fd61608543afb74a0f
koolkarkee <koolkarkee@gmail.com>
2024-06-06 09:20:47 +0545

    bfs traversal in graph

--- binarytree.c
+++ binarytree.c
@@ -34,9 +34,48 @@ struct node* create(){
     return newnode;
 }
 
+void preorder(struct node *root){ 
+    if(root == NULL){
+        return;
+    }
+
+    printf("%d ", root->data);
+
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(struct node *root){
+    if(root == NULL){
+        return;
+    }
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void postorder(struct node *root){
+    if(root == NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data); 
+}
+
 int main(){
     struct node *root = NULL;
     root = create();
+
+    printf("\nPreorder is : ");
+    preorder(root);
+
+    printf("\nInorder is : ");
+    inorder(root); 
+
+    printf("\nPostorder is : ");
+    postorder(root); 
 
     return 0;
 }
